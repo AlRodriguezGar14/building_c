90bfd37143740fcdfe7da1aaa6e79e6ca460da8c
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-20 14:22:42 +0100

    update: strlen function created. created a test for this type of function

---
+++ strlen.c
@@ -0,0 +1,9 @@
+unsigned long ft_strlen(const char *str) {
+    unsigned long idx;
+    
+    idx = 0;
+
+    while (str[idx] != 0)
+        idx++;
+    return idx;
+}
--- testing/test-functions.c
+++ testing/test-functions.c
@@ -1,11 +1,14 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "../isalnum.c"
 #include "../isalpha.c"
 #include "../isdigit.c"
 #include "../isascii.c"
 #include "../isprint.c"
+#include "../strlen.c"
 
 
 void print_pass_int(int original, int copy);
@@ -13,6 +16,29 @@ void print_fail_int(int original, int copy);
 void print_test_title(char *title);
 
 typedef int (*CharTypeChecker)(int);
+typedef size_t (*CheckStringContent)(const char*);
+
+void test_string_content(CheckStringContent original_function, CheckStringContent my_function, char* title)
+{
+    print_test_title(title);
+
+    char *tests[] = {
+        "Hello World",
+        "Numb3rs && Ch4rs",
+        "Str\twith\ttabs"
+    };
+
+    for (int i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+        int original= original_function(tests[i]);
+        int copy = my_function(tests[i]);
+
+        printf("input: '%s' - ", tests[i]);
+        if (original == copy)
+            print_pass_int(original, copy);
+        else
+            print_fail_int(original,copy);
+    }
+}
 
 void test_different_char_types(CharTypeChecker char_type_checker, CharTypeChecker my_checker, char *title)
 {
@@ -44,10 +70,15 @@ void test_different_char_types(CharTypeChecker char_type_checker, CharTypeChecke
 
 int main()
 {
+    // Char type functions
     test_different_char_types(&isdigit, &ft_isdigit, "Testing isdigit()");   
     test_different_char_types(&isalpha, &ft_isalpha, "Testing isalpha()");   
     test_different_char_types(&isalnum, &ft_isalnum, "Testing isalnum()");   
     test_different_char_types(&isascii, &ft_isascii, "Testing isascii()");   
     test_different_char_types(&isprint, &ft_isprint, "Testing isprint()");   
+
+
+    // Test length
+    test_string_content(&strlen, &ft_strlen, "Testing strlen()");
     return 0;
 }
