be628acbf8703359797c1183480120681616a822
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-20 17:56:55 +0100

    update: memset function created. test for memset created.
    optional todo: escalete the memset test to all string mutation tests

--- testing/printers.c
+++ testing/printers.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 #define COLOR_RED     "\x1b[31m"
 #define COLOR_GREEN   "\x1b[32m"
 #define COLOR_RESET   "\x1b[0m"
+
+
+void print_pass_str(char *original, char *copy)
+{
+    printf(COLOR_GREEN"expected: %s, result: %s. PASS\n"COLOR_RESET, original, copy);
+}
+
+void print_fail_str(char *original, char *copy)
+{
+    printf(COLOR_RED"expected: %s, result: %s. FAIL\n"COLOR_RESET, original, copy);
+}
+
+void    print_str_comparison(char *original, char *copy)
+{
+    if (strcmp(original, copy) == 0) {
+        print_pass_str(original, copy);
+        return;
+    }
+    print_fail_str(original, copy);
+}
+
 
 void print_pass_int(int original, int copy)
 {
@@ -12,6 +34,7 @@ void print_fail_int(int original, int copy)
 {
     printf(COLOR_RED"expected: %d, result: %d. FAIL\n"COLOR_RESET, original, copy);
 }
+
 
 void print_test_title(char *title) {
     printf("\n");
--- testing/test-functions.c
+++ testing/test-functions.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "../isalnum.c"
 #include "../isalpha.c"
@@ -9,14 +10,49 @@
 #include "../isascii.c"
 #include "../isprint.c"
 #include "../strlen.c"
+#include "../memset.c"
 
 
 void print_pass_int(int original, int copy);
 void print_fail_int(int original, int copy);
 void print_test_title(char *title);
+void    print_str_comparison(char *original, char *copy);
 
 typedef int (*CharTypeChecker)(int);
 typedef size_t (*CheckStringContent)(const char*);
+typedef void* (*MemSet)(void*, int, size_t);
+
+void test_manipulate_string(MemSet original_func, MemSet my_func, char* title)
+{
+    print_test_title(title);
+    char *tests_a[] = {
+        "Hello how are you doing?",
+        "I'm using multipl€ ic0ns & numbers",
+        "Add\tsome\ttabs"
+    };
+    
+    char *tests_b[] = {
+        "Hello how are you doing?",
+        "I'm using multipl€ ic0ns & numbers",
+        "Add\tsome\ttabs"
+    };
+
+    for (int i = 0; i < sizeof(tests_a) / sizeof(tests_a[0]); i++) {
+        printf("Input: '%s' - ", tests_a[i]);
+
+        char *a = malloc(strlen(tests_a[i]) + 1);
+        char *b = malloc(strlen(tests_b[i]) + 1);
+        strcpy(a, tests_a[i]);
+        strcpy(b, tests_b[i]);
+
+        original_func(a, '*', sizeof(char)*i);
+        my_func(b, '*', sizeof(char)*i);
+
+        print_str_comparison(a, b);
+        free(a);
+        free(b);
+    }
+}
 
 void test_string_content(CheckStringContent original_function, CheckStringContent my_function, char* title)
 {
@@ -32,7 +68,7 @@ void test_string_content(CheckStringContent original_function, CheckStringConten
         int original= original_function(tests[i]);
         int copy = my_function(tests[i]);
 
-        printf("input: '%s' - ", tests[i]);
+        printf("Input: '%s' - ", tests[i]);
         if (original == copy)
             print_pass_int(original, copy);
         else
@@ -80,5 +116,8 @@ int main()
 
     // Test length
     test_string_content(&strlen, &ft_strlen, "Testing strlen()");
+
+    // Manipulate content of strings
+    test_manipulate_string(&memset, &ft_memset, "Testing memset()");
     return 0;
 }
