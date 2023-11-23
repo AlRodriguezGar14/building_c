0054fae49ec3a60a4f88f39a764752b5621d6eec
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-23 05:01:20 +0100

    fix: changed the order of the conditional of the loop of strnstr. changed the location to the needle re-assignation. fix overflows and uncontrolled loops

--- strnstr.c
+++ strnstr.c
@@ -1,4 +1,6 @@
+#include <stddef.h>
 #include <stdio.h>
+#include "libft.h"
 
 char *ft_strnstr(const char *haystack, const char *needle, size_t loc)
 {
@@ -8,21 +10,21 @@ char *ft_strnstr(const char *haystack, const char *needle, size_t loc)
     if (*needle == '\0')
         return (char *)haystack;
 
-    while (haystack[idx] && idx < loc)
+    while (idx < loc && haystack[idx])
     {
-        idx++;
         match_idx = idx;
         if (haystack[idx] == *needle)
         {
             while (haystack[idx] == *needle && idx < loc)
             {
                 needle++;
-                if (*needle == '\0' || idx >= loc)
+                if (!*needle || idx >= loc)
                     return (char *)&haystack[match_idx];
                 idx++;
             }
+            needle -= (idx - match_idx); // reset needle in the case it is not a match
         }
-        needle -= (idx - match_idx); // reset needle in the case it is not a match
+        idx++;
     }
     return NULL;
 }
