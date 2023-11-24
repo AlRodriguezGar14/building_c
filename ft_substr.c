6832c9a333f6852aec6e79a785cc93dab19991d2
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-24 18:46:05 +0100

    ft_substr function created

--- ft_substr.c
+++ ft_substr.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "libft.h"
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    return (char*)s;
+    size_t idx = 0;
+    char *substr;
+    
+    if (len == 0 || start >= ft_strlen(s))
+    {
+        substr = (char *)malloc(sizeof(char));
+        *substr = '\0';
+        return substr;
+    }
+
+    substr = (char *)malloc(len + 1 * sizeof(char));
+    if (substr == NULL) return NULL;
+
+    while (idx < len)
+    {
+        substr[idx] = s[start];
+        idx++;
+        start++;
+    }
+    substr[idx] = '\0';
+
+    return substr;
 }
--- libft.h
+++ libft.h
@@ -31,5 +31,6 @@ char *ft_strchr(const char *str, int c);
 void *ft_memcpy(void *dest, void *src, size_t bytes_copied);
 int ft_atoi(const char *str);
 char *ft_substr(char const *s, unsigned int start, size_t len);
+char *ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
