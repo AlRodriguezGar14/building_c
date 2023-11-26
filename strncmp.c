b6bd3103622477860b0e68519e2a00297a897a68
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-26 21:13:39 +0100

    fix: strncmp returns 0 when there's not n to compare.

--- strncmp.c
+++ strncmp.c
@@ -5,6 +5,8 @@
 int ft_strncmp(const char *s1, const char *s2, size_t n)
 {
     size_t idx = 0;
+    if (!n)
+        return 0;
     while (idx < n - 1 && *s1 == *s2 && (*s1 != '\0' || *s2 != '\0'))
     {
         idx++;
