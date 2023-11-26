02b0d9174022e54ed965932a7ad3512eec3b4252
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-26 23:31:04 +0100

    fix: memcmp returns 0 if n is not-valid/null

--- memcmp.c
+++ memcmp.c
@@ -2,6 +2,9 @@
 
 int ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+    if (!n)
+        return 0;
+
     size_t idx = 0;
     const unsigned char *a = s1;
     const unsigned char *b = s2;
