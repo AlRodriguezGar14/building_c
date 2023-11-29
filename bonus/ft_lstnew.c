8f282d3dfa198444c1d8759e360b2d4385bbb0e6
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-11-29 02:25:24 +0100

    update: lstnew function created

--- bonus/ft_lstnew.c
+++ bonus/ft_lstnew.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../libft.h"
 
-void	ft_lstnew(void)
+t_list	*ft_lstnew(void *content)
 {
-	printf("hello\n");
+	t_list	*new_l;
+
+	new_l = malloc(sizeof(struct s_list));
+	if (!new_l)
+		return (NULL);
+	new_l->content = content;
+	new_l->next = NULL;
+	return (new_l);
 }
