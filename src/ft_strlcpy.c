476a612903f98f7b113404851807d1c1a098ae80
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2023-12-08 04:04:17 +0100

    update: used pointer arithmetic in strlcpy. The new version fails the norminette, so it's commented; even if cleaner and without magic numbers

--- src/ft_strlcpy.c
+++ src/ft_strlcpy.c
@@ -4,6 +4,22 @@
 #include <stdio.h>
 #include "../libft.h"
 
+// size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+// {
+// 	size_t	len;
+// 	size_t	idx;
+// 	int		sizeof_null;
+//
+// 	len = ft_strlen(src);
+// 	if (size <= 0)
+// 		return (len);
+// 	idx = 0;
+// 	sizeof_null = 1;
+// 	while ((idx++ < size - sizeof_null) && (*dest++ = *src++) != '\0')
+// 		*dest = '\0';
+// 	return (len);
+// }
+
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	idx;
@@ -21,3 +37,12 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	dest[idx] = '\0';
 	return (len);
 }
+
+// int main(void)
+// {
+//	   char    dest[10] = {0};
+//	   char    src[] = "Hello";
+//	   ft_strlcpy(dest, src, 3);
+//	   printf("dest: %s\n", dest);
+//	   return (0);
+// }
